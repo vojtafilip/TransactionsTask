6c51bef40c1f2c4abb93f99944d06ceb3a9ad54a
Vojta Filip <vojta@binarytao.com>
2026-01-08 21:07:48 +0100

    Compose tests setup

--- modules/webFronted/build.gradle.kts
+++ modules/webFronted/build.gradle.kts
@@ -33,6 +33,9 @@ kotlin {
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
+
+            @OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
+            implementation(compose.uiTest)
         }
     }
 }
--- modules/webFronted/src/webTest/kotlin/org/transactions_task/ComposeAppWebTest.kt
+++ modules/webFronted/src/webTest/kotlin/org/transactions_task/ComposeAppWebTest.kt
@@ -1,12 +1,27 @@
 package org.transactions_task
 
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.runComposeUiTest
 import kotlin.test.Test
-import kotlin.test.assertEquals
 
+@OptIn(ExperimentalTestApi::class)
 class ComposeAppWebTest {
 
     @Test
-    fun example() {
-        assertEquals(3, 1 + 2)
+    fun example() = runComposeUiTest {
+        // given
+        setContent { App() }
+        onNodeWithText("Hello", substring = true)
+            .assertDoesNotExist()
+
+        // when
+        onNodeWithText("Click me!")
+            .performClick()
+
+        // then
+        onNodeWithText("Hello", substring = true)
+            .assertExists()
     }
 }
